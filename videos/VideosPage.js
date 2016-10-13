d22041c7900b4eef7b80695699a31f98b097067e
Kevin Hou <kevin.ch.hou@gmail.com>
2016-10-12 20:14:03 -0400

    Added in grid for videos page

--- videos/VideosPage.js
+++ videos/VideosPage.js
@@ -2,14 +2,16 @@
 
 var VideosPage = React.createClass({
   getInitialState: function() {
+    console.log(videoData);
     return {
-      data: videoData
+      videoData: videoData
     }
   },
   render: function() {
     return (
       <div>
         <PageHeader />
+        <VideoGrid videoData={this.state.videoData} />
       </div>
     );
   }
@@ -18,14 +20,36 @@ var VideosPage = React.createClass({
 var PageHeader = React.createClass({
   render: function() {
     return (
-      <div>
-        Hello world
+      <div className="page-header">
+        <div className="page-header-title">
+          VIDEOS
+        </div>
       </div>
     )
   }
 })
 
-  React.render(
-    React.createElement(VideosPage, null),
-    document.getElementById('render-target')
-  );
+var VideoGrid = React.createClass({
+  render: function() {
+    var videoData = this.props.videoData;
+    console.log(videoData);
+    var videoNodes = videoData.map(function(video) {
+      return (
+        <div className="video-block">
+          <img className="video-thumbnail-img" src={video.thumbnailImage} alt="hello"/>
+        </div>
+      )
+    });
+
+    return (
+      <div className="video-grid">
+        {videoNodes}
+      </div>
+    )
+  }
+})
+
+React.render(
+  React.createElement(VideosPage, null),
+  document.getElementById('render-target')
+);
