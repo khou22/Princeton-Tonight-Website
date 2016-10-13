840626145e58d1c8237c3fa370f3b30400b82ecb
Kevin Hou <kevin.ch.hou@gmail.com>
2016-10-12 20:57:56 -0400

    Added video hover animation

--- videos/VideosPage.js
+++ videos/VideosPage.js
@@ -35,15 +35,51 @@ var VideoGrid = React.createClass({
     console.log(videoData);
     var videoNodes = videoData.map(function(video) {
       return (
-        <div className="video-block">
-          <img className="video-thumbnail-img" src={video.thumbnailImage} alt="hello"/>
-        </div>
+        <VideoBlock videoData={video} />
       )
     });
 
     return (
       <div className="video-grid">
         {videoNodes}
+      </div>
+    )
+  }
+})
+
+var VideoBlock = React.createClass({
+  getInitialState: function() {
+    return {
+      hovering: false
+    }
+  },
+  handleHoverOn: function() {
+    this.setState({
+      hovering: true
+    })
+  },
+  handleHoverOff: function() {
+    this.setState({
+      hovering: false
+    })
+  },
+  render: function() {
+    var video = this.props.videoData;
+
+    // Video hovering effects
+    var filter = this.state.hovering ? "video-fade" : "";
+    var descriptionOpacity = this.state.hovering ? "description-hover" : "";
+
+    return (
+      <div className="video-block"
+        onMouseEnter={this.handleHoverOn.bind(this)}
+        onMouseLeave={this.handleHoverOff.bind(this)}>
+        <img className={"video-thumbnail-img " + filter} src={video.thumbnailImage} alt="hello"/>
+        <div className="video-description-div">
+          <p className={"video-description " + descriptionOpacity}>
+            {video.description}
+          </p>
+        </div>
       </div>
     )
   }
