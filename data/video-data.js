fc02ebf8e858ce733cfbd94d3c2d017f30966883
Kevin Hou <kevin.ch.hou@gmail.com>
2016-10-30 17:35:36 -0400

    Finishing touches
    Added in mailchimp email service
    Removed contact section from header
    Finished video grid

--- data/video-data.js
+++ data/video-data.js
@@ -1,5 +1,20 @@
 console.log("Video data loaded");
+// Looks best with 6 videos
 var videoData = [
+  {
+    title: "Princeton Tonight S01E01 - Mike DelGuidice",
+    date: "February 27, 2016",
+    description: "Featuring an interview and performance with Mike DelGuidice (musician and Billy Joel band member). Plus an original sketch and \"These are Good Things\" with Princeton's own Paul Schorin",
+    youTubeLink: "https://www.youtube.com/watch?v=SXPdYRXzmY4",
+    thumbnailImage: "../data/video-thumbnails/S01E01.jpg"
+  },
+  {
+    title: "Princeton Tonight S01E01 - Mike DelGuidice",
+    date: "February 27, 2016",
+    description: "Featuring an interview and performance with Mike DelGuidice (musician and Billy Joel band member). Plus an original sketch and \"These are Good Things\" with Princeton's own Paul Schorin",
+    youTubeLink: "https://www.youtube.com/watch?v=SXPdYRXzmY4",
+    thumbnailImage: "../data/video-thumbnails/S01E01.jpg"
+  },
   {
     title: "Princeton Tonight S01E01 - Mike DelGuidice",
     date: "February 27, 2016",
