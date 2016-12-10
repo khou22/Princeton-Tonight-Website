16b9baa713abb220ab396ef5e7034ef0dffd2818
Kevin Hou <kevin.ch.hou@gmail.com>
2016-12-10 18:12:49 -0500

    Guests working on mobile and on dekstop

--- guests.js
+++ guests.js
@@ -11,17 +11,47 @@ var Guests = React.createClass({
     })
   },
   render: function() {
-    var max = 10; // max 4 guests listed
-    var currentGuest = guestData[this.state.index];
-    return (
-      <div className="guests">
-        <div className="guest-focus">
-          <h2 className="guest-name">{currentGuest.name}</h2>
-          <p className="guest-description">{currentGuest.description}</p>
+    // Check platform
+    if ($(window).width() >= 800) { // Desktop
+      var max = 10; // max 4 guests listed
+      var currentGuest = guestData[this.state.index];
+      return (
+        <div className="guests">
+          <div className="guest-focus">
+            <h2 className="guest-name">{currentGuest.name}</h2>
+            <p className="guest-description">{currentGuest.description}</p>
+          </div>
+          <GuestList guestData={guestData} numGuests={max} update={this.updateSelected}/>
         </div>
-        <GuestList guestData={guestData} numGuests={max} update={this.updateSelected}/>
-      </div>
-    )
+      )
+    } else { // Mobile
+      var i = 0; // Keep track of index
+      var guestLineup = guestData.map(function(guest) {
+        i++;
+        if (i == guestData.length) { // The last name
+          return (
+            <span>{guest.name}</span>
+          )
+        } else {
+          return (
+            <span>{guest.name}
+              <span className="guest-space">||||||</span>
+              ·
+              <span className="guest-space">||||||</span>
+            </span>
+          )
+        }
+      });
+
+      return (
+        <div className="guests-lineup-style">
+          <h2 className="lineup-header">Guests</h2>
+          <div className="lineup">
+            {guestLineup}
+          </div>
+        </div>
+      )
+    }
   }
 })
 
