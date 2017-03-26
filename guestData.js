8fe80e38be6698c74dce7578d36f660427f69cd6
Kevin Hou <kevin.ch.hou@gmail.com>
2017-03-25 21:42:05 -0700

    Added videos page experimentally

--- guestData.js
+++ guestData.js
@@ -12,46 +12,46 @@ var guestData = [
   {
     "name": "Mike Houston",
     "description": "Mike Houston is an actor best known for his role as prison guard Lee Dixon in the Netflix series Orange is the New Black. Houston is also a founding member of The Collective NY, alongside Amy Schumer, and has acted in Inside Amy Schumer as well as numerous other television, film, and theatrical productions.",
-    "image": "url(./assets/guests/MikeHouston.png)"
+    "image": "/assets/guests/MikeHouston.png"
   },
   {
     "name": "Charles Rogers and Jordan Firstman",
     "description": "Charles Rogers and Jordan Firstman came to Princeton Tonight to promote their new TBS series \"Search Party.\" Rogers, a co-creator, executive producer, director, and writer on the series, is also known for writing on the hit Netflix show Wet Hot American Summer. Firstman. a writer on Search Party, heroically stepped in to interview in place of Sarah-Violet Bliss, the other co-creator and executive producer, who fell ill after traveling. Since Rogers and Firstman appeared on the show, Search Party premiered to universal acclaim, including praise from GQ that called it \"the best show of the year.\" The creative team behind Search Party also held a public screening and Q&A at Princeton University following their interview.",
-    "image": "url(./assets/guests/RogersFirstman.jpg)"
+    "image": "/assets/guests/RogersFirstman.jpg"
   },
   {
     "name": "Gaby Moreno",
     "description": "Gaby Moreno is a Latin Grammy-winning singer/songwriter who visited Princeton as part of the Wilson College Lecture Series, in which she performed a set of original songs in Richardson Auditorium. Moreno, who wrote the theme song for the hit NBC sitcom Parks and Recreation and is currently the musical talent behind the first Latina Disney princess franchise Elena of Avalor, appeared on Princeton Tonight's Season 2 premiere episode to discuss her decorated career, as well as her new album Ilusión. Notably, Princeton Tonight conducted interviews with Moreno in English and Spanish, both of which can be found online.",
-    "image": "url(./assets/guests/GabyMoreno.png)"
+    "image": "/assets/guests/GabyMoreno.png"
   },
   {
     "name": "Lawrence (Band)",
     "description": "The band Lawrence, led by siblings Clyde and Gracie Lawrence, appeared on Princeton Tonight to perform an original set in the studio featuring hits from their new album, Breakfast, including \"Do You Wanna Do Nothing With Me?\" They also performed a special rendition of the Princeton Tonight theme song, which can be heard in the ending credits of every Princeton Tonight episode.",
-    "image": "url(./assets/guests/Lawrence.jpg)"
+    "image": "/assets/guests/Lawrence.jpg"
   },
   {
     "name": "Dave Coulier",
     "description": "An actor and comedian, Dave Coulier is best known for his role as Joey Gladstone in the hit 90s sitcom Full House. Coulier appeared on Princeton Tonight's Season 1 finale to discuss the first season of the Netflix reboot Fuller House, and presented his standup show to a sold-out crowd of over 500 students in McCosh 50 auditorium.",
-    "image": "url(./assets/guests/DaveCoulier.jpg)"
+    "image": "/assets/guests/DaveCoulier.jpg"
   },
   {
     "name": "John Caglione, Jr.",
     "description": "John Caglione, Jr. is an Academy Award-winning makeup artist known for his work on countless films including Dick Tracy and, more recently, The Dark Knight, in which he designed the makeup for Heath Ledger's The Joker character. Caglione discussed his celebrated career in his interview with Princeton Tonight, and held a public masterclass in which he showcased his work and answered questions from aspiring student artists.",
-    "image": "url(./assets/guests/JohnCaglioneJr.png)"
+    "image": "/assets/guests/JohnCaglioneJr.png"
   },
   {
     "name": "Rob Cohen and Colin Gray",
     "description": "Rob Cohen and Colin Gray are two award-winning filmmakers who came to Princeton to showcase their comedy-documentary film Being Canadian. Cohen, the film's director and host, is an accomplished television writer whose credits include The Big Bang Theory, The Simpsons, SNL, and more. Gray, the film's producer, has won numerous awards for his work on several documentaries, including Freedom's Fury. Cohen and Gray discussed the making of Being Canadian in a studio interview, played a game of Canadian Trivia (featuring maple syrup shots), and screened the film at the nearby Princeton Garden Theater with a Q&A following.",
-    "image": "url(./assets/guests/CohenGray.jpg)"
+    "image": "/assets/guests/CohenGray.jpg"
   },
   {
     "name": "Khalil Muhammad",
     "description": "Khalil Muhammad is an actor and comedian whose credits include Inside Amy Schumer and Trainwreck. Most notably, as a member of The Collective NY theater company, Muhammad is known for his standout one-man play, PRYOR TRUTH, an homage to the great comedian Richard Pryor. Muhammad premiered his show at Princeton University and spoke about his experiences as an actor in our studio interview. Since being featured at Princeton, PRYOR TRUTH has been showcased at a number of theater festivals across the country including New York Fringe, Hollywood Fringe, and the United Solo Theater Festival, in which it took the prestigious award for \"Best One Man Show.\"",
-    "image": "url(./assets/guests/PryorTruth.jpg)"
+    "image": "/assets/guests/PryorTruth.jpg"
   },
   {
     "name": "Mike DelGuidice",
     "description": "Mike DelGuidice is a self-taught musician who is currently touring with Billy Joel as a guitarist. His story is like no other: after leading his own Billy Joel cover group, Big Shot, for over 25 years, he got a call from the Piano Man himself to join his band. On Princeton Tonight's series premiere, Mike discussed his career and origins as a musician, including the story about being invited to join Billy Joel's band. He performed an original song, Days of Old, live in our studio, and held a free, public masterclass in Frist Theater, Princeton University—a brilliant show in which he accepted requests for both original and cover performances and spoke about his life and career. Since appearing on Princeton Tonight, Mike has been given the spotlight by Joel on tour, singing the famous opera Nessun Dorma in front of standing ovations across the world, and his original song \"Ordinary Guy\" was chosen as the theme song for the Kevin James CBS sitcom, \"Kevin Can Wait.\"",
-    "image": "url(./assets/guests/MikeDelGuidice.jpg)"
+    "image": "/assets/guests/MikeDelGuidice.jpg"
   }
 ];
