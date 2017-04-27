3274703506276190ea770d2d37abad6e96c54bc9
Kevin Hou <kevin.ch.hou@gmail.com>
2017-04-26 20:28:08 -0400

    Updated tile layout. Added in episode playlist data.

---
+++ _site/episodes/playlistData.js
@@ -0,0 +1,65 @@
+var playlistData = [
+  {
+    name: "Episodes",
+    subtitle: "Full SNL-style episodes",
+    slug: "https://www.youtube.com/playlist?list=PLbToYZfGuVo8pP2fNzpxja46JcRShEqgf",
+    image: "http://arts.princeton.edu/wp-content/uploads/2016/02/princeton-tonight.jpg",
+    category: "Episodes",
+    categoryColor: "red",
+    description: "Full episodes available for free on our YouTube page"
+  },
+  {
+    name: "Interviews",
+    subtitle: "With exclusive Princeton Tonight Guests",
+    slug: "https://www.youtube.com/playlist?list=PLbToYZfGuVo9QX0NtUT3m8vkQMEeLzJPW",
+    image: "http://arts.princeton.edu/wp-content/uploads/2016/02/princeton-tonight.jpg",
+    category: "Interviews",
+    categoryColor: "red",
+    description: "Princeton Tonight brings in and interviews celebrity guests."
+  },
+  {
+    name: "On The Street",
+    subtitle: "An original Princeton Tonight series.",
+    slug: "https://www.youtube.com/playlist?list=PLbToYZfGuVo8pP2fNzpxja46JcRShEqgf",
+    image: "http://arts.princeton.edu/wp-content/uploads/2016/02/princeton-tonight.jpg",
+    category: "Shorts",
+    categoryColor: "orange",
+    description: "A video short series featuring people on the street!"
+  },
+  {
+    name: "Sketches",
+    subtitle: "Original Princeton Tonight sketches.",
+    slug: "https://www.youtube.com/playlist?list=PLbToYZfGuVo9tq_tVshAh58MRoAcfIgwi",
+    image: "http://arts.princeton.edu/wp-content/uploads/2016/02/princeton-tonight.jpg",
+    category: "Shorts",
+    categoryColor: "orange",
+    description: "Sketches..."
+  },
+  {
+    name: "Promos",
+    subtitle: "Promo videos",
+    slug: "https://www.youtube.com/playlist?list=PLbToYZfGuVo-jh2HfQR0gX7OYt5wKyGjC",
+    image: "http://arts.princeton.edu/wp-content/uploads/2016/02/princeton-tonight.jpg",
+    category: "Unknown",
+    categoryColor: "orange",
+    description: "Promos..."
+  },
+  {
+    name: "These Are Good Things",
+    subtitle: "Unknown...",
+    slug: "https://www.youtube.com/playlist?list=PLbToYZfGuVo8az-falm4WuZ1es7qxZlfa",
+    image: "http://arts.princeton.edu/wp-content/uploads/2016/02/princeton-tonight.jpg",
+    category: "Unknown",
+    categoryColor: "orange",
+    description: "Unknown..."
+  },
+  {
+    name: "Rob Cohen an Colin Gray",
+    subtitle: "Unknown...",
+    slug: "https://www.youtube.com/playlist?list=PLbToYZfGuVo8xmm-Z1p82-qn5hsF_I046",
+    image: "http://arts.princeton.edu/wp-content/uploads/2016/02/princeton-tonight.jpg",
+    category: "Unknown",
+    categoryColor: "orange",
+    description: "Unknown..."
+  }
+]
--- _site/episodes/videos.js
+++ _site/episodes/videos.js
@@ -5,6 +5,7 @@ var gridData = {
   data: []
 }
 
+/************  Episodes  ************
 // Populate grid data from videoData object
 // Prepares the gridData object
 for (var i = 0; i < videoData.length; i++) {
@@ -20,6 +21,27 @@ for (var i = 0; i < videoData.length; i++) {
     category: {
       label: "Videos",
       color: "orange"
+    }
+  }
+
+  gridData.data.push(gridItem); // Add to data
+}
+*/
+
+/************  Playlists  ************/
+for (var i = 0; i < playlistData.length; i++) {
+  var currentPlaylist = playlistData[i];
+
+  var gridItem = {
+    title: currentPlaylist.name,
+    subtitle: currentPlaylist.subtitle,
+    description: currentPlaylist.description,
+    image: currentPlaylist.image,
+    link: currentPlaylist.slug,
+    size: "2",
+    category: {
+      label: currentPlaylist.category,
+      color: currentPlaylist.categoryColor
     }
   }
 
